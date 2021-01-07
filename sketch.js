ad58b66f5a0a11b519b253d5ce1abc0b6e0f4b69
HiyaDarwani <75484071+HiyaDarwani@users.noreply.github.com>
2021-01-07 08:58:16 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,27 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+function setup() {
+	createCanvas(800, 700);
+    engine = Engine.create();
+	  world = engine.world;
+    
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background("Black");
+
+
+  drawSprites();
+ //ground.display();
+}
+
+
+
+
